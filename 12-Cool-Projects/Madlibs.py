7663667c46142cc15d1c78e747fbb7483f761f3d
snigdhcinu <snigdhshourya11111998@gmail.com>
2020-12-12 00:11:41 +0530

    adding madlib project

---
+++ 12-Cool-Projects/Madlibs.py
@@ -0,0 +1,12 @@
+# Madlibs
+noun1 = input('Enter noun 1')
+verb1 = input('Enter verb 1')
+thing1 = input('Enter name of a thing')
+location1 = input('Enter a body part')
+
+string = f"""
+This is a story about a boy named {noun1} who loved {verb1} his friends' {thing1}, he would play with it, till his heart's desire
+and then would go to bed putting it inside his {location1}
+"""
+
+print(string)
