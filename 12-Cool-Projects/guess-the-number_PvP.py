d7102bc0ee32312fa893cb34109801427f79c4cb
snigdhcinu <snigdhshourya11111998@gmail.com>
2020-12-14 21:21:24 +0530

    improved some implementation logic in PvP mode

--- 12-Cool-Projects/guess-the-number_PvP.py
+++ 12-Cool-Projects/guess-the-number_PvP.py
@@ -2,19 +2,19 @@ import random
 
 def start():
     answer = input('Player-1 Enter a Number : ')
-    guess = input('Player-2 Guess the number : ')
     
     while(1):
+        guess = input('Player-2 Guess the number : ')
         if(guess == answer):
             print('✔ Awesome, Your guess was correct !! 🔥🔥🔥🔥')
-            return
+            break
+        elif(guess > answer):
+            print(' 👎 Try again with a smaller number ')
+            continue
         else:
-            if(guess > answer):
-                print(' 👎 Try again with a smaller number ')
-                break
-            else:
-                print('👎 Try again with a bigger number')
-                break
-                    
+            print('👎 Try again with a bigger number')
+            continue
+                 
+    return   
 
 start()
